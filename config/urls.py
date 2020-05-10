3fce6e825493a59b46627191cbcad273e704bd0a
Elya Manoukian <elya.manoukian@gmail.com>
2020-05-10 23:39:29 +0400

    E1 (#3)
    * Created custom authentication for only login and logout without registration and reset pwd, only admin can add user
    
    * Created custom authentication for only login and logout without registration and reset pwd, only admin can add user
    
    * included rights and auto_login columns to users table
    
    * Delete db.sqlite3
    
    Co-authored-by: Vagharsh Kandilian <vagharsh.kandilian@disqo.com>
    Co-authored-by: Vagharsh Kandilian <vagharsh@users.noreply.github.com>

--- config/urls.py
+++ config/urls.py
@@ -3,9 +3,15 @@ from django.contrib import admin
 from django.urls import path, include
 
 from tree.views import load_home
+from account.views import (
+    home_screen_view,
+    logout_view,
+    login_view,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('django.contrib.auth.urls')),
-    path('', load_home, name='home'),
+    path('', home_screen_view, name='home'),
+    path('logout', logout_view, name='logout'),
+    path('login', login_view, name='login'),
 ]
